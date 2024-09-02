fb70eab20bd0806b0ceeb05417c24f3c399c72de
ggsv06 <g278439@dac.unicamp.br>
2024-09-02 13:50:28 -0300

    base 05

---
+++ LABORATÓRIOS/05/lab05.py
@@ -0,0 +1,12 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 5 - Tit for Tat
+# Nome: Gian Gabriel Silva Vianna
+# RA: 278439
+###################################################
+
+# Leitura da entrada
+
+# Processamento dos dados
+
+# Saída de dados
