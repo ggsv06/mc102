fe39c280ac706383ea3033ae1877d33b1b351d87
ggsv06 <g278439@dac.unicamp.br>
2024-10-06 17:12:55 -0300

    "pc enzo"

---
+++ LABORATÓRIOS/10/lab10.py
@@ -0,0 +1,13 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 10 - Kungsleden
+# Nome: Gian Gabriel Silva Vianna
+# RA: 23062009
+###################################################
+
+
+# Leitura de dados
+
+# Processamento de dados
+
+# Saída de dados
